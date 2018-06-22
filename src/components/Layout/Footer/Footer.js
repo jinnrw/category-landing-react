6be90dc7e2a7488b6864c354dc68045ae3b8a336
kleung <kleung@pnimedia.com>
2018-06-21 17:25:16 -0700

    footer update

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -8,17 +8,46 @@ class Footer extends Component {
         <div className="footer-list">
           <ol className="footer-links">
             <li className="footer-subtitle"><span>Quick Links</span></li>
-            <li className="footer-link"><a href="">link</a></li>
-            <li className="footer-link"><a href="">link</a></li>
-            <li className="footer-link"><a href="">link</a></li>
-            <li className="footer-link"><a href="">link</a></li>
+            <li className="footer-link"><a href="#">My Photos</a></li>
+            <li className="footer-link"><a href="#">My Projects</a></li>
+            <li className="footer-link"><a href="#">Shop All Products</a></li>
+            <li className="footer-link"><a href="#">Mobile App</a></li>
+            <li className="footer-link"><a href="#">Group Rooms</a></li>
+            <li className="footer-link"><a href="#">Order Status</a></li>
+            <li className="footer-link"><a href="#">Image Galleries</a></li>
+            <li className="footer-link"><a href="#">Inspiration Blog</a></li>
+            <li className="footer-link"><a href="#">Help</a></li>
           </ol>
           <ol className="footer-links">
             <li className="footer-subtitle"><span>About Us</span></li>
-            <li className="footer-link"><a href="">link</a></li>
-            <li className="footer-link"><a href="">link</a></li>
-            <li className="footer-link"><a href="">link</a></li>
-            <li className="footer-link"><a href="">link</a></li>
+            <li className="footer-link"><a href="#">Contact Us</a></li>
+            <li className="footer-link"><a href="#">Pricing</a></li>
+            <li className="footer-link"><a href="#">Storage Policy</a></li>
+            <li className="footer-link"><a href="#">Privacy</a></li>
+            <li className="footer-link"><a href="#">Terms and Conditions</a></li>
+            <li className="footer-link"><a href="#">Print Profiles</a></li>
+            <li className="footer-link"><a href="#">Costco Business Printing</a></li>
+            <li className="footer-link"><a href="#">Costco.com</a></li>
+          </ol>
+          <ol className="footer-links">
+            <li className="footer-subtitle"><span>Products</span></li>
+            <li className="footer-link"><a href="#">Art &amp; Image Gallery</a></li>
+            <li className="footer-link"><a href="#">Calendars</a></li>
+            <li className="footer-link"><a href="#">Canvas Prints</a></li>
+            <li className="footer-link"><a href="#">Collage Prints</a></li>
+            <li className="footer-link"><a href="#">Disney Gallery</a></li>
+            <li className="footer-link"><a href="#">Greeting Cards</a></li>
+            <li className="footer-link"><a href="#">Metal Prints</a></li>
+          </ol>
+          <ol className="footer-links">
+            <li className="footer-subtitle"><span></span></li>
+            <li className="footer-link"><a href="#">Mugs</a></li>
+            <li className="footer-link"><a href="#">Photo Books</a></li>
+            <li className="footer-link"><a href="#">Photo USB &amp; DVD</a></li>
+            <li className="footer-link"><a href="#">Photo Plaques</a></li>
+            <li className="footer-link"><a href="#">Photo Throw</a></li>
+            <li className="footer-link"><a href="#">Poster Boards</a></li>
+            <li className="footer-link"><a href="#">Sports Gallery</a></li>
           </ol>
         </div>
       </footer>
