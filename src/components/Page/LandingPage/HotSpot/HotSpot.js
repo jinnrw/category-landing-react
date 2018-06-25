e90031448f0eec1cc6d5313124a0b791341c364d
kleung <kleung@pnimedia.com>
2018-06-25 12:21:04 -0700

    CSS animation for hotspots

--- src/components/Page/LandingPage/HotSpot/HotSpot.js
+++ src/components/Page/LandingPage/HotSpot/HotSpot.js
@@ -6,75 +6,77 @@ class HotSpot extends Component {
     render() {
         return(
             <div className="banner-hotspots">
-                <a className="background" href="/Shop"><img src={hotspotbanner} alt="" /></a>
-                <div className="hotspot hs1">
-                    <div className="dot-ref">
-                        <div className="dot"></div>
+                <div className="container">
+                    <a className="background" href="/Shop"><img src={hotspotbanner} alt="" /></a>
+                    <div className="hotspot hs1">
+                        <div className="dot-ref">
+                            <div className="dot"></div>
+                        </div>
+                        <div className="hotspot-modal">
+                            <figure>
+                                <img src="http://placehold.it/75x75" alt="" />
+                                <div className="details">
+                                    <figcaption>Tri-fold Brochures</figcaption>
+                                    <a href="#">Shop Brochures</a>
+                                </div>
+                            </figure>
+                        </div>
                     </div>
-                    <div className="hotspot-modal">
-                        <figure>
-                            <img src="http://placehold.it/75x75" alt="" />
-                            <div className="details">
-                                <figcaption>Tri-fold Brochures</figcaption>
-                                <a href="#">Shop Brochures</a>
-                            </div>
-                        </figure>
+                    <div className="hotspot hs2">
+                        <div className="dot-ref">
+                            <div className="dot"></div>
+                        </div>
+                        <div className="hotspot-modal">
+                            <figure>
+                                <img src="http://placehold.it/75x75" alt="" />
+                                <div className="details">
+                                    <figcaption>Brochures</figcaption>
+                                    <a href="#">Shop Brochures</a>
+                                </div>
+                            </figure>
+                        </div>
                     </div>
-                </div>
-                <div className="hotspot hs2">
-                    <div className="dot-ref">
-                        <div className="dot"></div>
+                    <div className="hotspot hs3">
+                        <div className="dot-ref">
+                            <div className="dot"></div>
+                        </div>
+                        <div className="hotspot-modal">
+                            <figure>
+                                <img src="http://placehold.it/75x75" alt="" />
+                                <div className="details">
+                                    <figcaption>Banners</figcaption>
+                                    <a href="#">Shop Banners</a>
+                                </div>
+                            </figure>
+                        </div>
                     </div>
-                    <div className="hotspot-modal">
-                        <figure>
-                            <img src="http://placehold.it/75x75" alt="" />
-                            <div className="details">
-                                <figcaption>Brochures</figcaption>
-                                <a href="#">Shop Brochures</a>
-                            </div>
-                        </figure>
+                    <div className="hotspot hs4">
+                        <div className="dot-ref">
+                            <div className="dot"></div>
+                        </div>
+                        <div className="hotspot-modal">
+                            <figure>
+                                <img src="http://placehold.it/75x75" alt="" />
+                                <div className="details">
+                                    <figcaption>Business Cards</figcaption>
+                                    <a href="#">Shop Cards</a>
+                                </div>
+                            </figure>
+                        </div>
                     </div>
-                </div>
-                <div className="hotspot hs3">
-                    <div className="dot-ref">
-                        <div className="dot"></div>
-                    </div>
-                    <div className="hotspot-modal">
-                        <figure>
-                            <img src="http://placehold.it/75x75" alt="" />
-                            <div className="details">
-                                <figcaption>Banners</figcaption>
-                                <a href="#">Shop Banners</a>
-                            </div>
-                        </figure>
-                    </div>
-                </div>
-                <div className="hotspot hs4">
-                    <div className="dot-ref">
-                        <div className="dot"></div>
-                    </div>
-                    <div className="hotspot-modal">
-                        <figure>
-                            <img src="http://placehold.it/75x75" alt="" />
-                            <div className="details">
-                                <figcaption>Business Cards</figcaption>
-                                <a href="#">Shop Cards</a>
-                            </div>
-                        </figure>
-                    </div>
-                </div>
-                <div className="hotspot hs5">
-                    <div className="dot-ref">
-                        <div className="dot"></div>
-                    </div>
-                    <div className="hotspot-modal">
-                        <figure>
-                            <img src="http://placehold.it/75x75" alt="" />
-                            <div className="details">
-                                <figcaption>Greeting Cards</figcaption>
-                                <a href="#">Shop Greeting Cards</a>
-                            </div>
-                        </figure>
+                    <div className="hotspot hs5">
+                        <div className="dot-ref">
+                            <div className="dot"></div>
+                        </div>
+                        <div className="hotspot-modal">
+                            <figure>
+                                <img src="http://placehold.it/75x75" alt="" />
+                                <div className="details">
+                                    <figcaption>Greeting Cards</figcaption>
+                                    <a href="#">Shop Greeting Cards</a>
+                                </div>
+                            </figure>
+                        </div>
                     </div>
                 </div>
             </div>
